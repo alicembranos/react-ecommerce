2c4bec11e545c999f9e4f2cc1b4adb41a26f020c
Alicia Cembranos <alicembru15@gmail.com>
2022-07-19 10:47:17 +0200

    redirection in checkout and got to shop button in cart component

--- src/components/ButtonsGroup/ButtonsGroup.jsx
+++ src/components/ButtonsGroup/ButtonsGroup.jsx
@@ -1,11 +1,35 @@
+import { useCallback, useContext } from "react";
+import { useLocation } from "wouter";
+import UserContext from "context/UserContext";
 import CustomButton from "components/CustomButton/CustomButton";
 import "./ButtonsGroup.css";
 
 const ButtonsGroup = () => {
+  const [, setLocation] = useLocation();
+  const { user } = useContext(UserContext);
+
+  const navigateToShop = useCallback(() => {
+    setLocation("/shop");
+  }, [setLocation]);
+
+  const navigateToLogin = useCallback(() => {
+    if (!Boolean(user)) {
+      setLocation("/login");
+    }
+    // setLocation("/chekcout");
+    console.log("checkout");
+  }, [setLocation, user]);
+
   return (
     <div className="buttons__container">
-      <CustomButton value={"Return to shop"}></CustomButton>
-      <CustomButton value={"Check out"}></CustomButton>
+      <CustomButton
+        value={"Return to shop"}
+        onClick={navigateToShop}
+      ></CustomButton>
+      <CustomButton
+        value={"Check out"}
+        onClick={navigateToLogin}
+      ></CustomButton>
     </div>
   );
 };
