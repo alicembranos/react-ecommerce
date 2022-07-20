fc8dcef48926f8ef1c64c8f6bd4594514d884ca5
Alicia Cembranos <alicembru15@gmail.com>
2022-07-21 00:55:16 +0200

    handle duration state and play/pause toggle function

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useRef, useEffect } from "react";
 import KeyboardArrowLeftOutlinedIcon from "@mui/icons-material/KeyboardArrowLeftOutlined";
 import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
 import PlayArrowOutlinedIcon from "@mui/icons-material/PlayArrowOutlined";
@@ -6,17 +6,46 @@ import PauseOutlinedIcon from "@mui/icons-material/PauseOutlined";
 import "./AudioPlayer.css";
 
 const AudioPlayer = () => {
+  //state
   const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
 
-  const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
+  //references
+  const audioPlayer = useRef();
+
+  const handleDuration = () => {
+    const seconds = Math.floor(audioPlayer?.current?.duration);
+    setDuration(seconds);
   };
 
+  const calculateTime = (secs) => {
+    console.log(secs);
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const seconds = Math.floor(minutes % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    return `${returnedMinutes}:${returnedSeconds}`;
+  };
+
+  const togglePlayPause = () => {
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioPlayer.current.play();
+    } else {
+      audioPlayer.current.pause();
+    }
+  };
+
+  console.log("esto es " + duration);
   return (
     <div className="audioPlayer">
       <audio
+        ref={audioPlayer}
         src="https://rascalsthemes.com/demo/vex/audio/Wok_Hard.mp3"
         preload="metadata"
+        onLoadedMetadata={handleDuration}
       ></audio>
       <button className="forwardBackward">
         <KeyboardArrowLeftOutlinedIcon fontSize="large" />
@@ -32,11 +61,13 @@ const AudioPlayer = () => {
         <KeyboardArrowRightOutlinedIcon fontSize="large" />
       </button>
 
-      <div className="currentTime">0:00</div>
+      <div className="currentTime">{calculateTime(currentTime)}</div>
       <div>
-        <input type="range" className="progressBar"/>
+        <input type="range" className="progressBar" defaultValue={0}/>
       </div>
-      <div className="duration">2:49</div>
+      <div className="duration">
+        {!isNaN(duration) && calculateTime(duration)}
+      </div>
     </div>
   );
 };
