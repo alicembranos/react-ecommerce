4e077a27c5aba643e3b1232be2f54288166a9887
Alicia Cembranos <alicembru15@gmail.com>
2022-09-01 18:08:40 +0200

    add new deploy from vercel

--- src/services/getEvents.js
+++ src/services/getEvents.js
@@ -2,7 +2,11 @@ import { API_URL_CONCERTS } from "./settings";
 
 const getEvents = async () => {
   try {
-    const response = await fetch(API_URL_CONCERTS);
+    const response = await fetch(API_URL_CONCERTS, {
+      headers: {
+        mode: "cors",
+      },
+    });
     if (response.ok) {
       const data = await response.json();
       return data;
