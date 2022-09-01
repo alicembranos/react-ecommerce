5503d401299bf0915efc807e227e803e7e53139d
Alicia Cembranos <alicembru15@gmail.com>
2022-09-01 18:10:07 +0200

    remove slash from urls setting file

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,5 +1,5 @@
-export const API_URL_ALBUMS = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app//albums";
-export const API_URL_IMAGES = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app//images";
-export const API_URL_USERS = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app//user/";
-export const API_URL = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app//";
-export const API_URL_CONCERTS = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app//concerts";
+export const API_URL_ALBUMS = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app/albums";
+export const API_URL_IMAGES = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app/images";
+export const API_URL_USERS = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app/user/";
+export const API_URL = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app/";
+export const API_URL_CONCERTS = "https://backend-ecommerce-du7glvgpu-alicembranos.vercel.app/concerts";
